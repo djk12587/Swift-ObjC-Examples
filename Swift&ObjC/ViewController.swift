7bcfa032ed0014be939290a3be2f107ab1bed2ae
Dan Koza <djk12587@gmail.com>
2014-08-04 13:57:55 -0500

    Initial Commit

---
+++ Swift&ObjC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift&ObjC
+//
+//  Created by Daniel Koza on 8/4/14.
+//  Copyright (c) 2014 Allstate R&D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
