fb0ab3a97c054fcaa850520f3b99bab376e6311d
Dan Koza <djk12587@gmail.com>
2014-08-07 15:28:04 -0500

    moved swift helper to the view controller

--- Swift&ObjC/CatViewControllerSwift.swift
+++ Swift&ObjC/CatViewControllerSwift.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+extension UIImage {
+    class func loadFromURL(url:NSURL, callBack:(image:UIImage) -> ()) {
+        
+        var callerQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)
+        
+        dispatch_async(callerQueue, {
+            var imageData = NSData.dataWithContentsOfURL(url, options: NSDataReadingOptions.DataReadingMappedAlways, error: nil)
+            dispatch_async(dispatch_get_main_queue(), {
+                var image = UIImage(data: imageData)
+                callBack(image: image)
+            })
+        })
+    }
+}
+
 class CatViewControllerSwift: CatViewControllerParent {
     
     override func viewDidLoad() {
